3be4581cf6c414f406335c31aeae360e292c1f38
Raphaël VERNETTI <raphael.vernetti@gmail.com>
2023-10-11 09:33:37 +0200

    button component

---
+++ components/atoms/button/button.atom.tsx
@@ -0,0 +1,24 @@
+import React from 'react'
+import styles from './button.module.css'
+
+interface Button {
+    onClick: React.MouseEventHandler<HTMLButtonElement>
+    children: React.ReactNode
+    style?: object
+}
+
+const Button = (props: Button) => {
+    const { onClick, children, style } = props
+
+    return (
+        <button 
+            className={styles.button} 
+            onClick={onClick} 
+            style={style}
+        >
+            {children}
+        </button>
+    )
+}
+
+export default Button
