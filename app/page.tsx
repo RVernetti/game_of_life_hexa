68d71a1b3dd89594d428d88e0fd39a2e4f4d7571
Raphaël VERNETTI <raphael.vernetti@gmail.com>
2023-10-18 15:18:51 +0200

    states embryo

--- app/page.tsx
+++ app/page.tsx
@@ -1,13 +1,19 @@
-import React from 'react'
+'use client'
+
+import React, { useState } from 'react'
 import styles from './page.module.css'
 import { Grid, Panel } from '@/components/molecules'
 
-const HomePage = () => (
+const HomePage = () => {
+  const [radius, setRadius] = useState(6)
+  const [isRunning, setRunning] = useState(false)
+
+  return (
   <div className={styles.layout}>
-    <Grid/>
+    <Grid radius={radius}/>
     <Panel/>
   </div>
-)
+)}
 
 export default HomePage
 
