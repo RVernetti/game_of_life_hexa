e9375f696f2c123fc0ea45032a004be7570a66d1
Raphaël VERNETTI <raphael.vernetti@gmail.com>
2023-10-25 15:15:14 +0200

    stores comments

--- stores/grid.store.js
+++ stores/grid.store.js
@@ -1,5 +1,6 @@
 import { atom } from 'recoil'
 
+// The adjustable grid radius
 export const radiusState = atom({
     key: 'radius-state', // unique ID (with respect to other atoms/selectors)
     default: 6, // default value (aka initial value)
