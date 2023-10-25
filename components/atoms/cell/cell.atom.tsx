4cbf8fca77c0d6214b6551759d286aff5689879a
Raphaël VERNETTI <raphael.vernetti@gmail.com>
2023-10-25 23:20:36 +0200

    layout responsability transfered to cell

--- components/atoms/cell/cell.atom.tsx
+++ components/atoms/cell/cell.atom.tsx
@@ -1,29 +1,46 @@
 'use client'
 
 import React from 'react'
-import styles from './cell.module.css'
 
-import { Hex } from '@/helpers/hex/hexagonal.helper'
+import { Point, Layout, Hex } from '@/helpers/hex/hexagonal.helper'
 
 import { useRecoilState } from 'recoil'
 import { cellStateFamily } from '@/stores/cell.store'
 
+import styles from './cell.module.css'
+
 interface Cell {
     coordinates: Hex
-    children?: React.ReactNode
+    size?: number
     style?: object
+    children?: React.ReactNode
 }
 
 const Cell = (props: Cell) => {
-    const { coordinates, children, style } = props
+    const { 
+        coordinates, 
+        size = 29,
+        style,
+        children,
+    } = props
 
     const [alive, setAlive] = useRecoilState(cellStateFamily({ ...coordinates }))
+
+    const origin = new Point(0, 0)
+    const hexSize = new Point(size, size)
+    const layout = new Layout(Layout.flat, hexSize, origin)
+    const {x, y} = layout.hexToPixel(coordinates)
 
     return (
         <button 
             className={alive ? styles.alive : styles.dead} 
             onClick={() => setAlive(!alive)}
-            style={style}
+            style={{
+                // TODO: write adjustment formula to recenter cell from its left top corner, regarding to its size
+                left: x - 25, // to recenter cell from its corner
+                top: y - 25, // to recenter cell from its corner
+                ...style,
+            }}
         >
             {children}
         </button>
--- components/molecules/grid/grid.molecule.tsx
+++ components/molecules/grid/grid.molecule.tsx
@@ -1,46 +1,24 @@
 import React from 'react'
 
-import { Cell } from '@/components/atoms'
-
-import { generateGrid } from '@/helpers/grid/grid.helper'
-import { Point, Layout, Hex } from '@/helpers/hex/hexagonal.helper'
-
 import { useRecoilValue } from 'recoil'
 import { radiusState } from '@/stores/grid.store'
 
+import { generateGrid } from '@/helpers/grid/grid.helper'
+import { Hex } from '@/helpers/hex/hexagonal.helper'
+
+import { Cell } from '@/components/atoms'
+
 import styles from './grid.module.css'
 
-interface ISquareCoordinates {
-  x: number,
-  y: number,
-}
-interface Grid {
-  origin?: ISquareCoordinates
-  hexSize?: ISquareCoordinates
-}
-
-const Grid = (props: Grid) => {
-  const {
-    origin = new Point(0, 0), 
-    hexSize = new Point(29, 29),
-  } = props
-
+const Grid = () => {
   const radius = useRecoilValue(radiusState)
   const grid = generateGrid(radius)
-  const layout = new Layout(Layout.flat, hexSize, origin)
-
   const display = grid.map((coordinates: Hex) => {
     const {q, r, s} = coordinates
-    const {x, y} = layout.hexToPixel(coordinates)
-
     return (
       <Cell
         key={`[q: ${q}, r: ${r}, s: ${s}]`}
         coordinates={coordinates}
-        style={{
-          left: x - 25, // to recenter cell from its corner
-          top: y - 25, // to recenter cell from its corner
-        }}
       />
     )
   })
