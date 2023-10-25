d462a6caf0c77f1f5f5278268c43b98f0e0e9e72
Raphaël VERNETTI <raphael.vernetti@gmail.com>
2023-10-25 11:16:07 +0200

    radius atom id renamed

--- stores/grid.store.js
+++ stores/grid.store.js
@@ -1,6 +1,6 @@
 import { atom } from 'recoil'
 
 export const radiusState = atom({
-    key: 'radius', // unique ID (with respect to other atoms/selectors)
+    key: 'radius-state', // unique ID (with respect to other atoms/selectors)
     default: 6, // default value (aka initial value)
 })
