420c0eb168b2fd2eacecbf8a286776b1ff2dd917
Raphaël VERNETTI <raphael.vernetti@gmail.com>
2023-10-26 18:34:59 +0200

    game speed store

--- stores/game.store.js
+++ stores/game.store.js
@@ -5,3 +5,8 @@ export const gameRunningState = atom({
     key: 'game-running-state', // unique ID (with respect to other atoms/selectors)
     default: false, // not running by default
 })
+
+export const gameSpeedState = atom({
+    key: 'game-speed-state',
+    default: 1,
+})
