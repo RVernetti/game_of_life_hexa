0785002647f4c6a92bb47db1b2f55c4c83d36a9f
Raphaël VERNETTI <raphael.vernetti@gmail.com>
2023-10-26 23:22:58 +0200

    use server panel

---
+++ components/molecules/runningButton/runningButton.molecule.tsx
@@ -0,0 +1,21 @@
+'use client'
+
+import React from 'react'
+
+import { useRecoilState } from 'recoil'
+import { gameRunningState } from '@/stores/game.store'
+
+import { Button } from '@/components/atoms'
+
+
+const RunningButton = () => {
+    const [running, setRunning] = useRecoilState(gameRunningState)
+
+    return (
+        <Button onClick={() => setRunning(!running)}>
+            {running ? 'Stop' : 'Play'}
+        </Button>
+    )
+}
+
+export default RunningButton
--- components/organisms/panel/panel.organism.tsx
+++ components/organisms/panel/panel.organism.tsx
@@ -1,28 +1,21 @@
-'use client'
-
 import React from 'react'
 
-import { useRecoilState } from 'recoil'
-import { gameRunningState } from '@/stores/game.store'
-
-import { Button } from '@/components/atoms'
-
 import styles from './panel.module.css'
-import { CellsRadiusInput, GameSpeedInput, GridRadiusInput } from '@/components/molecules'
 
-const Panel = () => {
-  const [running, setRunning] = useRecoilState(gameRunningState)
+import { 
+  CellsRadiusInput, 
+  GameSpeedInput, 
+  GridRadiusInput, 
+  RunningButton 
+} from '@/components/molecules'
 
-  return (
-    <div className={styles.panel}>
-      <GridRadiusInput />
-      <CellsRadiusInput />
-      <GameSpeedInput />
-      <Button onClick={() => setRunning(!running)}>
-        {running ? 'Stop' : 'Play'}
-      </Button>
-    </div>
-  )
-}
+const Panel = () => (
+  <div className={styles.panel}>
+    <GridRadiusInput />
+    <CellsRadiusInput />
+    <GameSpeedInput />
+    <RunningButton />
+  </div>
+)
 
 export default Panel
