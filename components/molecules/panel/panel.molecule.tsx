f9aec18f0f7ba7fcb49ffa4412b7ff9a1e84aac1
Raphaël VERNETTI <raphael.vernetti@gmail.com>
2023-10-26 07:10:06 +0200

    grid radius input

--- components/molecules/panel/panel.molecule.tsx
+++ components/molecules/panel/panel.molecule.tsx
@@ -3,19 +3,32 @@
 import React from 'react'
 import styles from './panel.module.css'
 
-import { Button } from '@/components/atoms'
+import { Button, Input } from '@/components/atoms'
 
 import { useRecoilState } from 'recoil'
 import { gameRunningState } from '@/stores/game.store'
+import { gridRadiusState } from '@/stores/grid.store'
 
 const Panel = () => {
   const [running, setRunning] = useRecoilState(gameRunningState)
+  const [gridRadius, setGridRadius] = useRecoilState(gridRadiusState)
+
+  const handleGridRadiusChange = (e: React.FormEvent<HTMLInputElement>) => {
+    const radius = parseInt((e.target as HTMLInputElement).value) || 0
+    return setGridRadius(radius)
+  }
   
   return (
     <div className={styles.panel}>
-        <Button onClick={() => setRunning(!running)}>
-          {running ? 'Stop' : 'Play'}
-        </Button>
+      <Input 
+        title='Grid radius:'
+        onChange={handleGridRadiusChange}
+        max={20}
+        defaultValue={gridRadius}
+      />
+      <Button onClick={() => setRunning(!running)}>
+        {running ? 'Stop' : 'Play'}
+      </Button>
     </div>
   )
 }
