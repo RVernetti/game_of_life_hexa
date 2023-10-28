7e99b44284036edb34139fc09e547cf5e4479bba
Raphaël VERNETTI <raphael.vernetti@gmail.com>
2023-10-28 09:13:16 +0200

    stop running on radius change

--- components/molecules/gridRadiusInput/gridRadiusInput.molecule.tsx
+++ components/molecules/gridRadiusInput/gridRadiusInput.molecule.tsx
@@ -4,15 +4,17 @@ import React from 'react'
 
 import { useRecoilState } from 'recoil'
 import { gridState } from '@/stores/grid.store'
+import { gameRunningState } from '@/stores/game.store'
 
 import { Input, Title } from '@/components/atoms'
 import { generateGrid } from '@/helpers/grid/grid.helper'
 
-
 const GridRadiusInput = () => {
   const [grid, setGrid] = useRecoilState(gridState)
+  const [running, setRunning] = useRecoilState(gameRunningState)
 
   const handleGridRadiusChange = (e: React.FormEvent<HTMLInputElement>) => {
+    setRunning(false)
     const radius: number = parseInt((e.target as HTMLInputElement).value) || 1
     // TODO: function to preserve ancient grid cells statuses instead of renew
     const newGrid = generateGrid(radius)
@@ -26,7 +28,7 @@ const GridRadiusInput = () => {
         id='grid-radius-input'
         onChange={handleGridRadiusChange}
         min='1'
-        max='40'
+        max='20'
         defaultValue={10}
       />
     </Title>
