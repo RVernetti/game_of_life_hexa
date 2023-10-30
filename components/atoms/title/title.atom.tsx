659457b28689e60d245afc229e2a11df50491ed7
Raphaël VERNETTI <raphael.vernetti@gmail.com>
2023-10-30 16:17:19 +0100

    title atom styles props

--- components/atoms/title/title.atom.tsx
+++ components/atoms/title/title.atom.tsx
@@ -4,15 +4,19 @@ import styles from './title.module.css'
 
 interface Title {
     title: string
+    layoutStyle?: object
+    titleStyle?: object
     children?: React.ReactNode
 }
 
 const Title = (props: Title) => {
-    const { title, children } = props
+    const { title, layoutStyle, titleStyle, children } = props
 
     return (
-        <div className={styles.layout}>
-            <span className={styles.title}>{title}</span>
+        <div className={styles.layout} style={layoutStyle}>
+            <span className={styles.title} style={titleStyle}>
+                {title}
+            </span>
             {children}
         </div>
     )
